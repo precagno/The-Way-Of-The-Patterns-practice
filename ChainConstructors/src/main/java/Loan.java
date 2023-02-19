c8ac09f74da98b0be92ea8efd5311d8190cc791f
pablo <pablosrecagno88@gmail.com>
2023-02-19 00:58:52 -0300

    Refactor chain constructors OK.

--- ChainConstructors/src/main/java/Loan.java
+++ ChainConstructors/src/main/java/Loan.java
@@ -1,53 +1,59 @@
 import java.util.Date;
 
 public class Loan {
-    private final CapitalStrategy strategy;
-    private float notional;
-    private float outstanding;
-    private int rating;
-    private Date start;
-    private Date expiry;
-    private Date maturity;
 
-    //This is the constructor for the Term Loan. It takes 2 parameters
-    public Loan(float notional, float outstanding, int rating, Date expiry) {
-        this.strategy = new TermLoanCapital();
-        this.notional = notional;
-        this.outstanding = outstanding;
-        this.rating = rating;
-        this.expiry = expiry;
-    }
+  private final CapitalStrategy strategy;
+  private float notional;
+  private float outstanding;
+  private int rating;
+  private Date start;
+  private Date expiry;
+  private Date maturity;
 
-    //This is the constructor for the Revolver Loan. It takes 3 parameters
-    public Loan(float notional, float outstanding, int rating, Date start, Date expiry) {
-        this.strategy = new RevolverCapital();
-        this.notional = notional;
-        this.outstanding = outstanding;
-        this.rating = rating;
-        this.start = start;
-        this.expiry = expiry;
-    }
+  //This is the constructor for the Term Loan. It takes 2 parameters
+  public Loan(final float notional, final float outstanding, final int rating, final Date expiry) {
 
-    //This is the constructor for the RCTL Loan. It takes 3 parameters
-    public Loan(float notional, float outstanding, int rating, Date start, Date expiry, Date maturity) {
-        this.strategy = new RCTLCapital();
-        this.notional = notional;
-        this.outstanding = outstanding;
-        this.rating = rating;
-        this.start = start;
-        this.expiry = expiry;
-        this.maturity = maturity;
-    }
+    this.baseConstructor(notional, rating, expiry);
+    this.strategy = new TermLoanCapital();
+    this.outstanding = outstanding;
+  }
 
-    //TODO: Add a description for this constructor
-    public Loan(CapitalStrategy strategy, float notional, float outstanding,
-                int rating, Date start, Date expiry, Date maturity) {
-        this.strategy = strategy;
-        this.notional = notional;
-        this.outstanding = outstanding;
-        this.rating = rating;
-        this.start = start;
-        this.expiry = expiry;
-        this.maturity = maturity;
-    }
+  //This is the constructor for the Revolver Loan. It takes 3 parameters
+  public Loan(final float notional, final float outstanding, final int rating, final Date start,
+      final Date expiry) {
+
+    this.baseConstructor(notional, rating, expiry);
+    this.strategy = new RevolverCapital();
+    this.outstanding = outstanding;
+    this.start = start;
+  }
+
+  //This is the constructor for the RCTL Loan. It takes 3 parameters
+  public Loan(final float notional, final float outstanding, final int rating, final Date start,
+      final Date expiry, final Date maturity) {
+
+    this.baseConstructor(notional, rating, expiry);
+    this.strategy = new RCTLCapital();
+    this.outstanding = outstanding;
+    this.start = start;
+    this.maturity = maturity;
+  }
+
+  //TODO: Add a description for this constructor
+  public Loan(final CapitalStrategy strategy, final float notional, final float outstanding,
+      final int rating, final Date start, final Date expiry, final Date maturity) {
+
+    this.baseConstructor(notional, rating, expiry);
+    this.strategy = strategy;
+    this.outstanding = outstanding;
+    this.start = start;
+    this.maturity = maturity;
+  }
+
+  private void baseConstructor(final float notional, final int rating, final Date expiry) {
+
+    this.notional = notional;
+    this.rating = rating;
+    this.expiry = expiry;
+  }
 }
